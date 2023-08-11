b5e9164932bb8e0c0a89d8ff884dea9a365573ea
aimner <aimner510@gmail.com>
2023-08-11 12:28:30 +0300

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,36 +1,48 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import s from './SuperButton.module.css'
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import s from "./SuperButton.module.css";
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
-type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
+type DefaultButtonPropsType = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: string
-}
+  xType?: string;
+};
 
-const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+const SuperButton: React.FC<SuperButtonPropsType> = ({
+  xType,
+  className,
+  disabled,
+  ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+}) => {
+  // задачка на смешивание классов
+
+  const finalClassName = () => {
+    let string = s.button;
+    if (!xType) {
+      string += " " + s.default;
     }
-) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    if (xType === "red") {
+      string += " " + s.red;
+    }
+    if (xType === "secondary") {
+      string += " " + s.secondary;
+    }
+    if (disabled) {
+      string = s.button + " " + s.disabled;
+    }
+    return string;
+  };
 
-    return (
-        <button
-            disabled={disabled}
-            className={finalClassName}
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        />
-    )
-}
+  return (
+    <button
+      disabled={disabled}
+      className={finalClassName()}
+      {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+    />
+  );
+};
 
-export default SuperButton
+export default SuperButton;
