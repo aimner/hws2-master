2c111488cee69de350dc704923ec05f2dc31f422
aimner <aimner510@gmail.com>
2023-09-12 11:34:29 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,22 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: "sort"; payload: "up" | "down" } | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
 
-            return state // need to fix
-        }
-        case 'check': {
+  switch (action.type) {
+    case "sort": {
 
-            return state // need to fix
-        }
-        default:
-            return state
+      if (action.payload === "up") {
+        return [...state].sort((a, b) =>a.name.localeCompare(b.name));
+      } else {
+        return [...state].sort((a, b) => b.name.localeCompare(a.name));
+      }
     }
-}
+    case "check": {
+      return [...state].filter((user) => user.age >= 18);
+    }
+    default:
+      return state;
+  }
+};
